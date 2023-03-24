7e369689a167d71c038f37a08e558d0548707fdf
Ayhan Metin <ayhanmet1@gmail.com>
2023-03-24 17:22:02 +0100

    update

---
+++ 03-diving-deeper/cypress/e2e/customQuery.cy.js
@@ -0,0 +1,13 @@
+
+
+describe('contract form', () => {
+it('should validate the form input', () => {
+  cy.visit('http://127.0.0.1:5173/about')
+
+  cy.getById('contact-btn-submit').click
+  cy.getById('contact-btn-submit').then((el) =>{
+    expect(el).to.not.have.attr('disabled')
+    expect(el.text()).does.not.eq('Sending...')
+    })
+  })
+});
